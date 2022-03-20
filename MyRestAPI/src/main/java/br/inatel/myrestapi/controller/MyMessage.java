75bc857a90c2e0f9e9686dd3c35f433c2b7d172f
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-03-20 14:38:52 -0300

    Aula Rest com spring

---
+++ MyRestAPI/src/main/java/br/inatel/myrestapi/controller/MyMessage.java
@@ -0,0 +1,12 @@
+package br.inatel.myrestapi.controller;
+
+public class MyMessage {
+	private String info;
+	
+	public String getInfo() {
+		return this.info;
+	}
+	public void setInfo(String info){
+		this.info=info;
+	}
+}
